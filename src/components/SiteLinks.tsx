bbd6aac16fac222019638603ba0061b481a4d077
Kyle Bennett <kylebennett00001@gmail.com>
2022-11-13 20:13:30 +0000

    Update 5 files

---
+++ src/components/SiteLinks.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+export const SiteLinks = () => {
+  return (
+    <div className="link-wrapper">
+      <a
+        href="https://www.linkedin.com/in/kyle-bennett-pittsburgh-pa/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          alt="linkedin"
+          height="32"
+          width="32"
+          src="http://etseq.law.harvard.edu/wp-content/uploads/2013/08/Screen-Shot-2013-08-11-at-6.39.25-PM.png"
+        />
+      </a>
+      <a href="https://codepen.io/coderkyle" target="_blank" rel="noreferrer">
+        <img
+          alt="codepen"
+          height="32"
+          width="32"
+          src="https://clipground.com/images/codepen-logo-8.png"
+        />
+      </a>
+      <a
+        href="https://github.com/BennEntterprise"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          alt="github"
+          height="32"
+          width="32"
+          src="https://toppng.com/uploads/thumbnail/github-mark-logo-vector-11573976116oq3oau9n10.png"
+        />
+      </a>
+      <a
+        href="https://www.twitch.tv/physicsphreak"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          alt="twitch"
+          height="32"
+          width="32"
+          src="https://i.pinimg.com/originals/c9/75/fe/c975fe8c491ce28c6081246c44a61e74.png"
+        />
+      </a>
+    </div>
+  );
+};
