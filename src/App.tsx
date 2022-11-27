a603cb3d6a87ea9d6dc9334a0ed842a80af8f428
Kyle Bennett <kylebennett00001@gmail.com>
2022-11-27 17:30:08 +0000

    Update 4 files

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { SiteLinks } from "./components/SiteLinks";
 import { LetterHead } from "./components/LetterHead";
 import { StickyFooter } from "./components/StickyFooter";
 import { Stacks } from "./components/Stacks";
+import { WordCloud } from "./components/WordCloud";
 
 // USING ICONS FROM A CDN
 export const App = () => {
   return (
     <div className="App">
-      <div className="card">
+      <WordCloud />
+      {/*  <div className="card">
         <LetterHead />
         <SiteLinks />
       </div>
-     {/* <Stacks />*/}
+      */}
+      {/* <Stacks />*/}
       <StickyFooter />
     </div>
   );
---
+++ src/components/WordCloud.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const WordCloud = () => {
+  return (
+    <div className="">
+      <ul>
+        <li>
+          <a href="https://open-props.style/">Open-Props by Adam Argyle</a>
+        </li>
+        <li>
+          <a href="https://www.pine64.org/">Pine64 Org</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
