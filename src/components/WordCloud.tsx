6bb4d1cdeb8c26646154bc483c21920803d9c929
Kyle Bennett <kylebennett00001@gmail.com>
2022-12-04 03:12:26 +0000

    feat: add some other stuff, like Mycroft and ray cast

--- src/components/WordCloud.tsx
+++ src/components/WordCloud.tsx
@@ -2,13 +2,26 @@ import React from "react";
 
 export const WordCloud = () => {
   return (
-    <div className="">
+    <div className="word-cloud--container">
+      <h5>Other Fun Stuff</h5>
       <ul>
         <li>
           <a href="https://open-props.style/">Open-Props by Adam Argyle</a>
         </li>
         <li>
           <a href="https://www.pine64.org/">Pine64 Org</a>
+        </li>
+        <li>
+          <a href="#">Raycast</a>
+        </li>
+        <li>
+          <a href="#">MycroftAI (Mark II)</a>
+        </li>
+         <li>
+          <a href="#">Conventional Commits</a>
+        </li>
+        <li>
+          <a href="https://keepachangelog.com/en/1.0.0/">Keep a Changelog</a>
         </li>
       </ul>
     </div>
