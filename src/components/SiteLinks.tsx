1f61f32b4d383d53310af70fc7f7bf0e43b47009
@BennEntterprise <kylebennett00001@gmail.com>
2023-03-03 02:06:40 -0500

    refactor: utilize a list and map pattern instead of hand entry.

--- src/components/SiteLinks.tsx
+++ src/components/SiteLinks.tsx
@@ -1,43 +1,44 @@
-import React from "react";
-import LinkedInLogo from "../images/LinkedInLogo.png";
+import React from 'react';
 
 export const SiteLinks = () => {
+  const links = [
+    {
+      href: 'https://www.linkedin.com/in/kyle-bennett-pittsburgh-pa/',
+      target: '_blank',
+      rel: 'noreferrer',
+      imgSrc: '/images/LinkedInLogo.png',
+      imgAlt: 'linkedin',
+    },
+    {
+      href: 'https://codepen.io/coderkyle',
+      target: '_blank',
+      rel: 'noreferrer',
+      imgSrc: 'images/codepen-logo.png',
+      imgAlt: 'codepen',
+    },
+    {
+      href: 'https://github.com/BennEntterprise',
+      target: '_blank',
+      rel: 'noreferrer',
+      imgSrc: 'images/github.png',
+      imgAlt: 'github',
+    },
+    {
+      href: 'https://www.twitch.tv/physicsphreak',
+      target: '_blank',
+      rel: 'noreferrer',
+      imgSrc: 'images/twitch.png',
+      imgAlt: 'twitch',
+    },
+  ];
+
   return (
     <div className="link-wrapper">
-      <a
-        href="https://www.linkedin.com/in/kyle-bennett-pittsburgh-pa/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img
-          alt="linkedin"
-          height="32"
-          width="32"
-          src="/images/LinkedInLogo.png"
-        />
-      </a>
-      <a href="https://codepen.io/coderkyle" target="_blank" rel="noreferrer">
-        <img
-          alt="codepen"
-          height="32"
-          width="32"
-          src="images/codepen-logo.png"
-        />
-      </a>
-      <a
-        href="https://github.com/BennEntterprise"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img alt="github" height="32" width="32" src="images/github.png" />
-      </a>
-      <a
-        href="https://www.twitch.tv/physicsphreak"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img alt="twitch" height="32" width="32" src="images/twitch.png" />
-      </a>
+      {links.map((link) => (
+        <a href={link.href} target={link.target} rel={link.rel}>
+          <img alt={link.imgAlt} height="32" width="32" src={link.imgSrc} />
+        </a>
+      ))}
     </div>
   );
 };
