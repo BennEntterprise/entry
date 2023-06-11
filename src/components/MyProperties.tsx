664a9445bfdc33f9a5aa8c2a878afd6a77128e04
Kyle Bennett <kylebennett00001@gmail.com>
2023-06-11 20:32:56 +0000

    add actual hyperlinks to the display
    (This requires rewriting the domains list with a bit more data like a
    URL and a description.)
    There's also a mechanism where I can "hide" certain items from view

--- src/components/MyProperties.tsx
+++ src/components/MyProperties.tsx
@@ -1,12 +1,23 @@
 // A React component that displays a list of domain names that I own.
 import React from 'react'
+import { IDomain } from '../App';
 
 interface IMyProperties {
-    domainNames: Array<string>;
+    domainNames: Array<IDomain>;
 }
+
+const excludedFromPresentation = [
+    'wussickcontracting.com',
+    'kylebennett.design',
+]
 
 export const MyProperties= (props:IMyProperties ) => {
     return (<div>
-        {props.domainNames.map(dn =><li>{dn}</li>)}
+        {props.domainNames.filter((d => {
+            if (excludedFromPresentation.includes(d.name)) {
+                return false;
+            }
+            return d;
+        })).map(dn =><li><a href={dn.url } rel='noreferrer'>{dn.name}</a></li>)}
     </div>)
 }
