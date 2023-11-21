2627d9f52b9013e493a921beb6e2217fc0be4cb5
Kyle Bennett <kylebennett00001@gmail.com>
2023-11-21 12:58:32 -0500

    Feat/posthog (#23)
    * doc: update readm
    * feat: install posthog
    * chore: add .gitignore and a sample.env file
    * feat: test implementation of posthog
    * try: remove 'process.env' from react code, there is no process for the FE

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ You can see the live deployment at [kylebennett.io](https://kylebennett.io)
 
 On or around Sat October 21, 2023 it looked like this.
 <img alt='photo of the kylebennett.io landing page' src='./public/images/HeroImage.png' width='50%'/>
+
+# Getting it Running Locally
+
+1. Clone/Fork the Repo
+2. `yarn i` to get the node_modules dependencies
+3. `yarn start` to start the react app.
+
+# Testing
+
+You can run tests with `yarn test` to run unit tests (currently a no-op)
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,37 @@
+
+import posthog from 'posthog-js'
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
+import { PostHogProvider } from "posthog-js/react";
+
+// Verify the posthog key is set, (not undefined)
+// if it is undefined, then we kill the process
+if (process.env.REACT_APP_PUBLIC_POSTHOG_KEY) {
+    console.log("Posthog Key is Set");
+} else {
+    console.log("Posthog Key is Not Set");
+    // Close the react app
+    process.env.REACT_APP_PUBLIC_POSTHOG_KEY='unset'
+}
+
+posthog.init(
+    process.env.REACT_APP_PUBLIC_POSTHOG_KEY,
+    {
+        api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+    }
+);
+
 const root = createRoot(document.getElementById("app"));
-root.render(<App />);
+
+root.render(
+    <React.StrictMode>
+        <PostHogProvider client={posthog}>
+            <App />
+        </PostHogProvider>
+    </React.StrictMode>
+);
 
 // TODO: Remove the Problematic .d.ts File. (Not sure which one)
