80793ed7d2d02662c63fc1c608c72e5136a9a503
Kyle Bennett <kylebennett00001@gmail.com>
2024-04-13 14:49:34 -0400

    fix contrast in the footer (#26)

--- src/components/StickyFooter.tsx
+++ src/components/StickyFooter.tsx
@@ -3,11 +3,11 @@ import React from "react";
 export const StickyFooter = () => {
   const [stickyFooterIndex, setStickyFooterIndex] = React.useState(0)
   
-  const github = <p>Made with ❤️ Using <a href='https://github.com'>Github"</a></p>;
-  const changelog = <p>Has a change log inspired by <a href='https://keepachangelog.com/en/1.0.0/'> keepachangelog.com</a></p>;
+  const github = <p>Made with ❤️ Using <a href='https://github.com' className='footer-link'>Github</a></p>;
+  const changelog = <p>Has a change log inspired by <a href='https://keepachangelog.com/en/1.0.0/' className='footer-link'> keepachangelog.com</a></p>;
   // const watchdog = <p>Watchdog services provided by <a href='https://snyk.io/'>Snyk</a></p>;
-  const conventionalCommits = <p>Commits follow <a href='https://www.conventionalcommits.org/en/v1.0.0/'>conventional commits</a></p> 
-  const frontendLogging = <p>Error and Exception handling gracefully traiged using <a href='Sentry.io'>Sentry</a></p> 
+  const conventionalCommits = <p>Commits follow <a href='https://www.conventionalcommits.org/en/v1.0.0/' className='footer-link'>conventional commits</a></p> 
+  const frontendLogging = <p>Error and Exception handling gracefully traiged using <a href='Sentry.io' className='footer-link'>Sentry</a></p> 
   
   
   const options = [
